13a5fc128bdb6c32d85fe75e4636f9b7d5c507f9
leannethng <leanne.thng@gmail.com>
2020-03-25 14:14:26 -0700

    Got the output to update but hard coded in info

--- bread.js
+++ bread.js
@@ -22,9 +22,12 @@ function saveInputs(e){
   e.preventDefault(); //To stop the form submitting
   
   // Saving over the values of each stage with the values entered into the input
-  breadSteps.prep = prepTime.value,
-  breadSteps.bulkFerm = bulkFermTime.value,
-  
+  breadSteps.prep = prepTime.value;
+  breadSteps.bulkFerm = bulkFermTime.value;
+
+
+  output.innerHTML = prepTime.value;
+  output.style.width = (prepTime.value * 10) + "%";
   console.log(breadSteps);
   
   // returning the object with new values
@@ -40,22 +43,21 @@ console.log(breadSteps);
 
 //-------
 // Try to write out the data this with be the method for the event handler
-function generateTimeline(data){
-  const addDiv = document.createElement('p');
-  addDiv.innerText = data.bulkFerm;
-// for (value in breadSteps){  
-// const addDiv = document.createElement('p');
+// function generateTimeline(data){
+//   const addDiv = document.createElement('p');
+//   addDiv.innerText = data.bulkFerm;
+// // for (value in breadSteps){  
+// // const addDiv = document.createElement('p');
+// // }
+// console.log(addDiv);
+// return addDiv;
 // }
-console.log(addDiv);
-return addDiv;
-}
 
-const steps = generateTimeline(breadSteps);
+// const steps = generateTimeline(breadSteps);
 
-output.insertAdjacentElement('afterbegin',steps );
+// output.insertAdjacentElement('afterbegin',steps );
 
-
-
+console.log(output)
 
 // I need to listen for the inputs for the creation of the divs too so they will update. 
 
