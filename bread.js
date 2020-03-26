15065473d0844352287636eec30b8b892d33374a
Treasure <treasure.rose@gmail.com>
2020-03-26 01:33:39 -0700

    remove unused code

--- bread.js
+++ bread.js
@@ -1,6 +1,3 @@
-// Grabbing the button
-const inputButton = document.querySelector(".inputButton");
-
 //creating div bars for timeline
 const output = document.querySelector(".output");
 
@@ -58,54 +55,3 @@ function updateView(event) {
 document.addEventListener("DOMContentLoaded", createView, false);
 // Listening for input
 inputs.forEach(input => input.addEventListener("input", updateView));
-
-// Method to add to the button when clicked
-// This will be adding the inputs to an object
-// function saveInputs(event) {
-//   //e.preventDefault(); //To stop the form submitting
-//   // console.log(time);
-
-//   breadSteps.forEach(step => {
-//     output.innerHTML += `<div class="${step.id} timeLineItem" style='width: ${step.time}'>
-//         <p>${step.name}</p>
-//         <p class="time">${step.time}</p>
-//       </div>`;
-
-//     if (step.id === e.target.id) {
-//       step.time = e.target.value;
-//       updateBreadSteps(step);
-//     }
-//   });
-// for (let i = 0; i < breadSteps.length; i++){
-//   // if the id of the object matches the input name
-//   if(breadSteps[i].id === e.target.id ){
-//     breadSteps[i].time = e.target.value;
-//     // console.log(breadSteps);
-//   }
-
-//   // breadSteps[i].time =
-//   // console.log(`${breadSteps[i].name}: ${breadSteps[i].time}`);
-// };
-
-// // console.log(e.target.id);
-// console.log(breadSteps);
-// return breadSteps;
-//}
-
-// function updateBreadSteps(){
-//   return breadSteps.forEach(step => {
-//     let something =
-//     `<div class="${step.id} timeLineItem" style='width: ${step.time}'>
-//       <p>${step.name}</p>
-//       <p>${step.time}</p>
-//     </div>
-//     `;
-//   })
-// };
-
-//function updateBreadSteps(step) {
-//console.log(step);
-//const time = document.querySelector(`.prepTime .time`);
-//console.log(time);
-// time.innerHTML = step.time;
-//}
