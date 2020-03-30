e2d110c703cafc6787eccb1337b51dee7d59f7eb
leannethng <leanne.thng@gmail.com>
2020-03-30 13:32:14 -0700

    Hide timeline when no values assigned and am/pm working

--- bread.js
+++ bread.js
@@ -34,51 +34,67 @@ function createView() {
 
   // Counter to help cycle through am/pm and to loop through 1-12
   let counter = 0;
+  let am = "am"
+  let pm = "pm"
+
+
   // Clear the timeLine.innerHTML so it refreshes rather than appends when looped through
   timeLine.innerHTML = "";
 
+
+
   // Create timeline elements, likely li items for i in timeLineHours create a li
   for(let i = 0; i < timeLineHours; i++){ 
+
+    //  amPm sets if the time is am or pm
+    let amPm = isEven(counter)
+    console.log(`amPm: ${amPm}`);
+
     if(`${i + startTime}` <= 12){
-    timeLine.innerHTML += `<li class="hour">${i + startTime}</li>`; } 
+    timeLine.innerHTML += `<li class="hour">${i + startTime}${amPm}</li>`; } 
     else {
       timeLine.innerHTML += `<li class="hour">${(i + 
       // Using the counter to flatten back down to 1-12
-      startTime-(12 * `${counter}`))}</li>`;
+      startTime-(12 * `${counter}`))}${amPm}</li>`;
     };
     // Set counter
     if(`${i + startTime}` % 12 === 0){
       counter ++; 
     };
-  };
 
-  // using modulo to check for am or pm
-  // need to them append the word am or pm to the end of the li
-  // also need a radio button to determine if the user is starting on am or pm then the other one
-  if(counter % 2 === 0){
-    console.log(`counter: ${counter}`);
-     console.log(`am`); 
-  } else {
-    console.log(`counter: ${counter}`);
-    console.log(`pm`); 
+    // using modulo to check for am or pm
+    // made an am/pm function so I can return the values to use above
+    function isEven(value){
+      if (value % 2 === 0)
+          return am;
+      else
+          return pm;
+    }
 };
+
   
   //Clear output div
   output.innerHTML = "";
   //Map through each object in data structure and return template 
   const view = breadSteps.map(step => {
-    return `<div class="${step.id} timeLineItem" style='width: ${step.time * 2}%'>
+    console.log(`stepTime: ${step.time}`)
+    if (step.time === 0){
+      return `<div class="${step.id} timeLineItem" style='display: none;'>
+    </div>`;
+    } else {
+    return `<div class="${step.id} timeLineItem" style='width: ${step.time * 4}%'>
               <div class="timeLineBlock"></div>
               <p>${step.name}</p>
               <p>${step.time}hr/s</p>
             </div>`;
+    }
   });
 
   // Add each template to the DOM
   view.forEach(breadStep => {
     output.innerHTML += breadStep;
   });
-}
+};
 
 function updateView(event) {
   // Loop through data structure, look for step that matches target, update time
