cd6c482714e6ad3b1957045a74267279f7e8a496
leannethng <leanne.thng@gmail.com>
2020-03-30 11:51:50 -0700

    added in am/pm tracker and small amount of styling to bar

--- bread.js
+++ bread.js
@@ -1,11 +1,9 @@
-
 //creating li items for timeline
 const timeLine = document.querySelector(".timing");
 //creating div bars for timeline
 const output = document.querySelector(".output");
 //Grabbing the input values
 const inputs = Array.from(document.querySelectorAll('[name="breadInputs"]'));
-
 
 //Dataset
 let breadSteps = [
@@ -28,43 +26,49 @@ let breadSteps = [
 
 let startTime = 1;
 
-
 function createView() {
   // Summing the total hours to create the timeline
   var timeLineHours = breadSteps.reduce(function(prev, cur) {
     return prev + cur.time;
   }, 0);
 
+  // Counter to help cycle through am/pm and to loop through 1-12
   let counter = 0;
-  // Create timeline elements, likely li items for i in timeLineHours create a li
+  // Clear the timeLine.innerHTML so it refreshes rather than appends when looped through
   timeLine.innerHTML = "";
-  for(let i = 0; i < timeLineHours; i++){ 
- 
-    if(`${i + startTime}` <= 12){
-    // console.log(`i: ${i} startTime: ${startTime}`); 
-    timeLine.innerHTML += `<li class="hour">${i + startTime}</li>`;
-    // } else if (`${i + startTime}` <= 24) {
-    //   timeLine.innerHTML += `<li class="hour">${(i + startTime)-12}</li>`;  
-    // } else if (`${i + startTime}` <= 48) {
-    //   timeLine.innerHTML += `<li class="hour">${(i + startTime-(12 * 2))}</li>`; 
-    // } 
-    } else {
-      console.log(`counter: ${counter}`);
-      timeLine.innerHTML += `<li class="hour">${(i + startTime-(12 * `${counter}`))}</li>`;
-    };
 
+  // Create timeline elements, likely li items for i in timeLineHours create a li
+  for(let i = 0; i < timeLineHours; i++){ 
+    if(`${i + startTime}` <= 12){
+    timeLine.innerHTML += `<li class="hour">${i + startTime}</li>`; } 
+    else {
+      timeLine.innerHTML += `<li class="hour">${(i + 
+      // Using the counter to flatten back down to 1-12
+      startTime-(12 * `${counter}`))}</li>`;
+    };
+    // Set counter
     if(`${i + startTime}` % 12 === 0){
       counter ++; 
     };
   };
 
-
-
+  // using modulo to check for am or pm
+  // need to them append the word am or pm to the end of the li
+  // also need a radio button to determine if the user is starting on am or pm then the other one
+  if(counter % 2 === 0){
+    console.log(`counter: ${counter}`);
+     console.log(`am`); 
+  } else {
+    console.log(`counter: ${counter}`);
+    console.log(`pm`); 
+};
+  
   //Clear output div
   output.innerHTML = "";
   //Map through each object in data structure and return template 
   const view = breadSteps.map(step => {
     return `<div class="${step.id} timeLineItem" style='width: ${step.time * 2}%'>
+              <div class="timeLineBlock"></div>
               <p>${step.name}</p>
               <p>${step.time}hr/s</p>
             </div>`;
